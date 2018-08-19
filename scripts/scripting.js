2719b035085311910a92e5eafa1fa4ecd53740fc
tootalls <42502929+tootalls@users.noreply.github.com>
2018-08-19 15:57:48 -0700

    meh

---
+++ scripts/scripting.js
@@ -0,0 +1,21 @@
+function myFunction() {
+    document.getElementById("demo1").innerHTML = "Hello Dolly!";
+    document.getElementById("demo2").innerHTML = "How are you?";
+}
+
+// hey look a comment
+
+/* whoah
+multiple line
+comments
+*/
+var a, b, c;     // Declare 3 variables
+a = 5;           // Assign the value 5 to a
+b = 6;           // Assign the value 6 to b
+c = a + b;       // Assign the sum of a and b to c
+
+var price1 = 5;
+var price2 = 6;
+var total = price1 + price2;
+
+var person = "John Doe", carName = "Volvo", price = 200;
